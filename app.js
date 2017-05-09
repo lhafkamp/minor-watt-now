ad181a09f325cac8ce3e58ff3c5f1b15323dedbc
Merlijn Vos <merlijn.vos043@gmail.com>
2017-05-09 11:40:41 +0200

    :hankey: Read mock csv

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+const fs = require('fs');
 const path = require('path');
 const http = require('http').Server;
 const express = require('express');
 const socketio = require('socket.io');
+const parse = require('csv-parse');
 
 const app = express();
 const server = http(app);
@@ -11,6 +13,18 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '/public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+fs.readFile('./mock.csv', {columns: ['timestamp', 'average', 'min', 'max']}, (err, data) => {
+  if (err) {
+    throw err;
+  }
+  parse(data, (err, output) => {
+    if (err) {
+      throw err;
+    }
+    console.log(output);
+  });
+});
 
 app.get('/', (req, res) => {
   res.render('index');
