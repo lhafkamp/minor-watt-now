4eacf6ad2710d00f1a4384c13e0a0c0a1deefb11
Merlijn Vos <merlijn.vos043@gmail.com>
2017-05-09 18:42:27 +0200

    :sparkles: Target newest 10 measurements

--- app.js
+++ app.js
@@ -9,26 +9,45 @@ const app = express();
 const server = http(app);
 const io = socketio(server);
 const port = process.env.PORT || 3000;
+let arr = [];
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-fs.readFile('./mock.csv', {columns: ['timestamp', 'average', 'min', 'max']}, (err, data) => {
+fs.readFile('./src/data/mock.csv', (err, data) => {
   if (err) {
     throw err;
   }
-  parse(data, (err, output) => {
-    if (err) {
-      throw err;
+  parse(data, {columns: ['timestamp', 'average', 'min', 'max']}, (error, output) => {
+    if (error) {
+      throw error;
     }
-    console.log(output);
+    interval(output);
   });
 });
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+function interval(data) {
+  let i = -1;
+  tick();
+
+  function tick() {
+    i++;
+    if (data[i]) {
+      algorithm(data[i]);
+      setTimeout(tick, 1000);
+    }
+  }
+}
+
+function algorithm(item) {
+  arr = arr.slice(-9).concat(item);
+  console.log(arr);
+}
 
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
