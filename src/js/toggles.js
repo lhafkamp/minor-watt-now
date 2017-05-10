e2e123d0bbf9ae484a0472e9804c6360fe1d50e6
lhafkamp <luukhafkamp@gmail.com>
2017-05-10 13:28:17 +0200

    :sparkles: set up for sending JSON messages to the client with sockets

--- src/js/toggles.js
+++ src/js/toggles.js
@@ -1,6 +1,7 @@
 const newArray = [];
 const spanId = localStorage.getItem('oldMessages');
 
+// localStorage to check which messages have already been seen
 if (localStorage.getItem('oldMessages') === null) {
   console.log('no local storage found');
 } else {
@@ -14,6 +15,7 @@ if (localStorage.getItem('oldMessages') === null) {
   });
 }
 
+// removing the 'new' indicator and send it to localStorage
 function messageSeen(e) {
   if (e.target.nodeName === 'SUMMARY') {
     const oldMessage = e.target.querySelector('span');
@@ -32,30 +34,3 @@ document.body.addEventListener('click', messageSeen);
 window.addEventListener("hashchange", () => {
     window.scrollTo(window.scrollX, window.scrollY - 60);
 });
-
-
-// TEST TIMEOUT FOR RECEIVING NEW MESSAGES (with audio)
-const advice = document.querySelector('#advice');
-const audio = document.querySelector('audio');
-
-// hack to force-play audio on mobile
-document.addEventListener('touchstart', () => {
-    audio.play();
-    audio.pause();
-});
-
-setTimeout(() => {
-audio.play();
-advice.insertAdjacentHTML('afterbegin',
-  `<article>
-    <details>
-      <summary>Nieuw bericht<span id="1" class="new"></span></summary>
-      <div>
-        <h2>67%</h2>
-        <p>Tijd om een andere generator aan te zetten</p>
-      </div>
-    </details>
-  </article>
-`)
-}, 3000);
-
