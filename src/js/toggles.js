0e2ceb3321693c8d4dd89e74c635aedbb43780f3
lhafkamp <luukhafkamp@gmail.com>
2017-05-10 11:05:34 +0200

    :sparkles: added audio on incoming message
    It doesn’t work on mobile so I used a hack that makes sure the audio
    plays once the user touched his screen once.

--- src/js/toggles.js
+++ src/js/toggles.js
@@ -1,5 +1,4 @@
 const newArray = [];
-const messages = document.querySelectorAll('details');
 const spanId = localStorage.getItem('oldMessages');
 
 if (localStorage.getItem('oldMessages') === null) {
@@ -15,18 +14,48 @@ if (localStorage.getItem('oldMessages') === null) {
   });
 }
 
-function messageSeen() {
-  const oldMessage = this.children[0].children[0];
-  oldMessage.classList.remove('new');
+function messageSeen(e) {
+  if (e.target.nodeName === 'SUMMARY') {
+    const oldMessage = e.target.querySelector('span');
+    oldMessage.classList.remove('new');
 
-  newArray.push(spanId);
-  newArray.push(oldMessage.id)
+    newArray.push(spanId);
+    newArray.push(oldMessage.id)
 
-  localStorage.setItem('oldMessages', newArray);
+    localStorage.setItem('oldMessages', newArray);
+  }
 }
 
-messages.forEach(message => message.addEventListener('click', messageSeen));
+document.body.addEventListener('click', messageSeen);
 
+// adjust navigation because of the fixed nav
 window.addEventListener("hashchange", () => {
     window.scrollTo(window.scrollX, window.scrollY - 60);
 });
+
+
+// TEST TIMEOUT FOR RECEIVING NEW MESSAGES (with audio)
+const advice = document.querySelector('#advice');
+const audio = document.querySelector('audio');
+
+// hack to force-play audio on mobile
+document.addEventListener('touchstart', () => {
+    audio.play();
+    audio.pause();
+});
+
+setTimeout(() => {
+audio.play();
+advice.insertAdjacentHTML('afterbegin',
+  `<article>
+    <details>
+      <summary>Nieuw bericht<span id="1" class="new"></span></summary>
+      <div>
+        <h2>67%</h2>
+        <p>Tijd om een andere generator aan te zetten</p>
+      </div>
+    </details>
+  </article>
+`)
+}, 3000);
+
