a6cbfb96685b673312f9244e29ae9c25297ab2d1
Luuk Hafkamp <luukhafkamp@gmail.com>
2017-05-11 11:25:50 +0200

    :sparkles: Removing messages by comparing the JSON type to elements (#4)

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ fs.readFile('./src/data/messages.json', (err, data) => {
 io.on('connection', socket => {
   setTimeout(() => {
     socket.emit('newMessage', simData);
-  }, 3000);
+  }, 1000);
 
   setTimeout(() => {
     socket.emit('newMessage', upData);
@@ -93,6 +93,10 @@ io.on('connection', socket => {
   setTimeout(() => {
     socket.emit('newMessage', downData);
   }, 8500);
+
+  setTimeout(() => {
+    socket.emit('removeMessage', upData);
+  }, 10000);
 });
 
 server.listen(port, () => {
