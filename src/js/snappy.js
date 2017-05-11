297c41c793a2549e2b42b9760c713258de4c768f
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2017-05-12 00:51:53 +0200

    :sparkles: markers based on types (#8)

--- src/js/snappy.js
+++
@@ -1,9 +0,0 @@
-const FTScroller = require('ftscroller');
-
-const container = document.querySelector('#generators');
-
-scroller = new FTScroller(container, {
-    scrollbars: false,
-    scrollingX: false,
-    scrollingClassName: 'generators-container'
-,});
