d07f020891053ec6320939ee6ae075c2f63f4fe9
lhafkamp <luukhafkamp@gmail.com>
2017-05-11 16:09:12 +0200

    :bug: localStorage only activates when he can find the element

--- app.js
+++ app.js
@@ -49,11 +49,6 @@ app.get('/generator', (req, res) => {
   res.render('generator');
 });
 
-app.get('/history', (req, res) => {
-  const derp = 'werwer';
-  res.render('history', {derp});
-});
-
 function interval(data) {
   let i = -1;
   tick();
